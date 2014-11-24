24482df875c639a46ec1c1405a4ca80637f7b49c
Jean Francis Dominique S. Mabalot <jmabalot@google.com>
2014-11-24 21:34:16 +0100

    update bash script Signed-off-by: Jean Francis Dominique S. Mabalot <jmabalot@google.com>

--- ramdisk/sbin/stuxnet.sh
+++ ramdisk/sbin/stuxnet.sh
@@ -6,13 +6,9 @@
 # busybox
 bb=/sbin/bb/busybox;
 
-# Disable MPD
-	$bb stop mpdecision
-	$bb echo "1" > /sys/module/intelli_plug/enabled
-
 # CPU Settings
         $bb echo "384000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-        $bb echo "1728000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+        $bb echo "1512000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
         $bb echo "Configure CPU Settings" | tee /dev/kmsg
 
 # SuperSU daemonsu/Superuser su_daemon
